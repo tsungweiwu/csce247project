5398ed905a8eb39a7d26a6b01b75bbaf4d5d4f4f
tsungweiwu <tsungweiwu@gmail.com>
2020-04-08 23:05:56 -0400

    AdminTest change

--- csce247FinalProject/src/test/java/project/objects/AdminTest.java
+++ csce247FinalProject/src/test/java/project/objects/AdminTest.java
@@ -2,32 +2,49 @@ package project.objects;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class AdminTest {
 
-    @BeforeEach
-    void setUp() {
-    }
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
 
-    @AfterEach
-    void tearDown() {
-    }
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
 
-    @Test
-    void getUsername() {
-        Admin admin = new Admin("username", "password");
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testGetUsername() {
+		Admin admin = new Admin("username", "password");
         assertEquals("username", admin.getUsername());
-    }
+	}
 
-    @Test
-    void getPassword() {
-    }
+	@Test
+	void testAdmin() {
+		fail("Not yet implemented");
+	}
 
-    @Test
-    void setPassword() {
-    }
+	@Test
+	void testGetPassword() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetPassword() {
+		fail("Not yet implemented");
+	}
+
 }
